b094ad888a5835d58f67ca6c134a9bb25cdd8597
Radoslav Valach <rado.valach@gmail.com>
2017-08-24 00:37:04 +0200

    Fixed bug in blame yourself

--- src/commands/fun/blame.js
+++ src/commands/fun/blame.js
@@ -6,7 +6,7 @@ import { rob } from '../../data';
 function blame(client, evt, suffix, lang) {
   const rand = Math.floor(Math.random() * rob.length);
   let robQuote = `${rob[rand]}`;
-  let selfQuote = robQuote.replace('Rob', evt.message.author.username);
+  let selfQuote = robQuote.replace(/Rob/gi, evt.message.author.username);
   return Promise.resolve(selfQuote);
 }
 
